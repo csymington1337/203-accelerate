26fd8f20972515dddecec49320dcb315fcfa5b60
csymington1337 <symingtonchristina@gmail.com>
2021-04-18 01:10:21 -0600

    Structural and styling changes to fix alignment of recent blog post

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -18,21 +18,24 @@ get_header(); ?>
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php the_content(); ?>
-				<a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
+					<a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
-	
+
+	<!-- RECENT BLOG POST -->
 	<section class="recent-posts">
- 		<div class="site-content"></div>
-		<div class="blog-post">
-			<h4>From the Blog</h4>
-			<?php query_posts('posts_per_page=1'); ?>
-  				<?php while ( have_posts() ) : the_post(); ?>
-    				<h3><?php the_title(); ?></h3>
-					<?php the_excerpt(); ?>
-  				<?php endwhile; ?> 
-			<?php wp_reset_query(); ?>
+		<div class="site-content">
+			<div class="blog-post">
+				<h4>From the Blog</h4>
+				<?php query_posts('posts_per_page=1'); ?>
+					<!-- the loop -->
+					<?php while ( have_posts() ) : the_post(); ?>
+						<h3><?php the_title(); ?></h3>
+						<?php the_excerpt(); ?>
+					<?php endwhile; // end of the loop. ?>
+				<?php wp_reset_query(); // resets the altered query back to the original ?>
+			</div>
 		</div>
 	</section>
 
